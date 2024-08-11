e205ab217ed155b3e8d69e793f729428dd811346
Zipkyn <v.t.s.782003@gmail.com>
2024-08-11 17:39:18 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sorted_students = sorted(students)
+average_grades = {student: sum(grade) / len(grade) for student, grade in zip(sorted_students, grades)}
+print(average_grades)
